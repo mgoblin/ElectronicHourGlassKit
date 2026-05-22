3b88776c40f3fc7bd0b43d200e641afd0b770d26
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-22 21:27:13 +0300

    Top LED triangle added to EHGK Editor main form

--- src/utils/ui_editor/lazarus/ehgk_editor/src/main.pas
+++ src/utils/ui_editor/lazarus/ehgk_editor/src/main.pas
@@ -5,10 +5,41 @@ unit Main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, SimplifiedLed, Forms, Controls, Graphics, Dialogs;
 
 type
+
+  { TMainForm }
+
   TMainForm = class(TForm)
+    LED8: TSimplifiedLed;
+    LED17: TSimplifiedLed;
+    LED18: TSimplifiedLed;
+    LED19: TSimplifiedLed;
+    LED20: TSimplifiedLed;
+    LED21: TSimplifiedLed;
+    LED22: TSimplifiedLed;
+    LED23: TSimplifiedLed;
+    LED24: TSimplifiedLed;
+    LED25: TSimplifiedLed;
+    LED26: TSimplifiedLed;
+    LED9: TSimplifiedLed;
+    LED27: TSimplifiedLed;
+    LED28: TSimplifiedLed;
+    LED1: TSimplifiedLed;
+    LED2: TSimplifiedLed;
+    LED3: TSimplifiedLed;
+    LED4: TSimplifiedLed;
+    LED5: TSimplifiedLed;
+    LED6: TSimplifiedLed;
+    LED7: TSimplifiedLed;
+    LED10: TSimplifiedLed;
+    LED11: TSimplifiedLed;
+    LED12: TSimplifiedLed;
+    LED13: TSimplifiedLed;
+    LED14: TSimplifiedLed;
+    LED15: TSimplifiedLed;
+    LED16: TSimplifiedLed;
   private
 
   public
