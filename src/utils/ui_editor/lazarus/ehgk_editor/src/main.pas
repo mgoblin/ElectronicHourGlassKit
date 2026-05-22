88317687e6518c73037cea2f18708e72ff392973
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-22 22:07:31 +0300

    All LEDs placed on main form in ehgk editor

--- src/utils/ui_editor/lazarus/ehgk_editor/src/main.pas
+++ src/utils/ui_editor/lazarus/ehgk_editor/src/main.pas
@@ -12,6 +12,35 @@ type
   { TMainForm }
 
   TMainForm = class(TForm)
+    LED29: TSimplifiedLed;
+    LED30: TSimplifiedLed;
+    LED31: TSimplifiedLed;
+    LED32: TSimplifiedLed;
+    LED33: TSimplifiedLed;
+    LED34: TSimplifiedLed;
+    LED35: TSimplifiedLed;
+    LED36: TSimplifiedLed;
+    LED37: TSimplifiedLed;
+    LED38: TSimplifiedLed;
+    LED39: TSimplifiedLed;
+    LED40: TSimplifiedLed;
+    LED41: TSimplifiedLed;
+    LED42: TSimplifiedLed;
+    LED43: TSimplifiedLed;
+    LED44: TSimplifiedLed;
+    LED45: TSimplifiedLed;
+    LED46: TSimplifiedLed;
+    LED47: TSimplifiedLed;
+    LED48: TSimplifiedLed;
+    LED49: TSimplifiedLed;
+    LED50: TSimplifiedLed;
+    LED51: TSimplifiedLed;
+    LED52: TSimplifiedLed;
+    LED53: TSimplifiedLed;
+    LED54: TSimplifiedLed;
+    LED55: TSimplifiedLed;
+    LED56: TSimplifiedLed;
+    LED57: TSimplifiedLed;
     LED8: TSimplifiedLed;
     LED17: TSimplifiedLed;
     LED18: TSimplifiedLed;
@@ -52,6 +81,9 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TMainForm }
+
 
 end.
 
