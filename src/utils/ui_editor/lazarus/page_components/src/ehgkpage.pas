4f5b01a427c0b20d8fc370777c7c78af24f8df2f
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-23 09:41:31 +0300

    Add bit manipulation procedures EHGK Page component

--- src/utils/ui_editor/lazarus/page_components/src/ehgkpage.pas
+++ src/utils/ui_editor/lazarus/page_components/src/ehgkpage.pas
@@ -8,23 +8,26 @@ uses
   Classes, SysUtils;
 
 type
-  Led = (
-      LED1 = 0,
-      LED2 = 1,
-      LED3 = 2
-  );
-  LedSet = Set of LED;
+
+  EIndexOutOfBounds = class(Exception);
+
+  { TEhgkPage }
 
   TEhgkPage = class(TComponent)
   private
-    FLEDs: LedSet;
+    FValue: UInt64;
+    function GetLed(Index: UInt8): Boolean;
+    procedure SetLed(Index: UInt8; AValue: Boolean);
   protected
+    procedure SetValueBit(BitIndex: UInt8); inline;
+    procedure ClearValueBit(BitIndex: UInt8); inline;
+    function IsValueBitSet(BitIndex: UInt8): Boolean; inline;
 
   public
+    property LED[Index: UInt8]: Boolean read GetLed write SetLed;
 
   published
-    property LEDS: LedSet read FLEDs write FLEDs;
-
+    property Value: UInt64 read FValue write FValue;
   end;
 
 procedure Register;
@@ -34,6 +37,44 @@ implementation
 procedure Register;
 begin
   RegisterComponents('EHGK',[TEhgkPage]);
+end;
+
+{ TEhgkPage }
+
+procedure TEhgkPage.SetValueBit(BitIndex: UInt8);
+begin
+  FValue := FValue or (1 shl BitIndex);
+end;
+
+procedure TEhgkPage.ClearValueBit(BitIndex: UInt8);
+begin
+  FValue := FValue and not (1 shl BitIndex);
+end;
+
+function TEhgkPage.IsValueBitSet(BitIndex: UInt8): Boolean;
+begin
+  Result := (FValue or (1 shl BitIndex)) <> 0;
+end;
+
+function TEhgkPage.GetLed(Index: UInt8): Boolean;
+begin
+  {TODO Implement me}
+  Result := False;
+end;
+
+procedure TEhgkPage.SetLed(Index: UInt8; AValue: Boolean);
+begin
+  if (Index < 1) or (Index > 57) then
+     raise EIndexOutOfBounds.CreateFmt('Index %d is out of bounds [1..57]', [Index]);
+
+  if AValue then
+  begin
+    SetValueBit(Index - 1);
+  end
+  else
+  begin
+    ClearValueBit(Index - 1);
+  end;
 end;
 
 end.
