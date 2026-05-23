5fa6aea71bfe459c800509ad87a6f62fb1f7689d
Michael Golovanov <mike.golovanov@gmail.com>
2026-05-23 13:38:54 +0300

    EHGK Page Contol TEhgkPageValue.LedByIndexOn added

--- src/utils/ui_editor/lazarus/page_components/src/ehgkpage.pas
+++ src/utils/ui_editor/lazarus/page_components/src/ehgkpage.pas
@@ -1,33 +1,40 @@
 unit EhgkPage;
 
 {$mode ObjFPC}{$H+}
+{$modeswitch typehelpers}{$R+}
 
 interface
 
 uses
   Classes, SysUtils;
 
+const
+  EhgkLedCountMax = 57;
+  EhgkPageValueMax = $01FFFFFFFFFFFFFF;
+
 type
 
   EIndexOutOfBounds = class(Exception);
+
+  TEhgkPageIndex = 1..EhgkLedCountMax;
+  TEhgkPageValue = 0..EhgkPageValueMax;
+
+  { TEhgkPageValueHelper }
+
+  TEhgkPageValueHelper = Type Helper for TEhgkPageValue
+  public
+    function LedByIndexOn(const Index: TEhgkPageIndex): TEhgkPageValue;
+  end;
 
   { TEhgkPage }
 
   TEhgkPage = class(TComponent)
   private
-    FValue: UInt64;
-    function GetLed(Index: UInt8): Boolean;
-    procedure SetLed(Index: UInt8; AValue: Boolean);
-  protected
-    procedure SetValueBit(BitIndex: UInt8); inline;
-    procedure ClearValueBit(BitIndex: UInt8); inline;
-    function IsValueBitSet(BitIndex: UInt8): Boolean; inline;
-
-  public
-    property LED[Index: UInt8]: Boolean read GetLed write SetLed;
+    FValue: TEhgkPageValue;
 
   published
-    property Value: UInt64 read FValue write FValue;
+    property Value: TEhgkPageValue read FValue write FValue;
+
   end;
 
 procedure Register;
@@ -39,42 +46,18 @@ begin
   RegisterComponents('EHGK',[TEhgkPage]);
 end;
 
+{ TEhgkPageValueHelper }
+
+function TEhgkPageValueHelper.LedByIndexOn(const Index: TEhgkPageIndex): TEhgkPageValue; inline;
+begin
+     Result := UInt64(Self).SetBit(Index - 1);
+end;
+
+{ TEhgkPageValueHelper }
+
 { TEhgkPage }
 
-procedure TEhgkPage.SetValueBit(BitIndex: UInt8);
-begin
-  FValue := FValue or (1 shl BitIndex);
-end;
 
-procedure TEhgkPage.ClearValueBit(BitIndex: UInt8);
-begin
-  FValue := FValue and not (1 shl BitIndex);
-end;
-
-function TEhgkPage.IsValueBitSet(BitIndex: UInt8): Boolean;
-begin
-  Result := (FValue or (1 shl BitIndex)) <> 0;
-end;
-
-function TEhgkPage.GetLed(Index: UInt8): Boolean;
-begin
-  {TODO Implement me}
-  Result := False;
-end;
-
-procedure TEhgkPage.SetLed(Index: UInt8; AValue: Boolean);
-begin
-  if (Index < 1) or (Index > 57) then
-     raise EIndexOutOfBounds.CreateFmt('Index %d is out of bounds [1..57]', [Index]);
-
-  if AValue then
-  begin
-    SetValueBit(Index - 1);
-  end
-  else
-  begin
-    ClearValueBit(Index - 1);
-  end;
-end;
+{ TEhgkPage }
 
 end.
--- src/utils/ui_editor/lazarus/page_components/test/testcases.pas
+++ src/utils/ui_editor/lazarus/page_components/test/testcases.pas
@@ -1,6 +1,7 @@
 unit TestCases;
 
 {$mode objfpc}{$H+}
+{$R+}
 
 interface
 
@@ -9,9 +10,22 @@ uses
 
 type
 
-  TCreateEhgkPageTestCase= class(TTestCase)
+  { TCreateEhgkPageTestCase }
+
+  TCreateEhgkPageTestCase = class(TTestCase)
   published
     procedure TestCreate;
+  end;
+
+  { TBitsEhgkPageTestCase }
+
+  TBitsEhgkPageTestCase = class(TTestCase)
+  protected
+    EhgkPage: TEhgkPage;
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestLedOn;
   end;
 
 implementation
@@ -24,10 +38,44 @@ begin
   AssertNotNull('EhgkPage should be not nil after creation', EhgkPage);
 end;
 
+{ TBitsEhgkPageTestCase }
+
+procedure TBitsEhgkPageTestCase.SetUp;
+begin
+  EhgkPage := TEhgkPage.Create(Nil);
+end;
+
+procedure TBitsEhgkPageTestCase.TearDown;
+begin
+  FreeAndNil(EhgkPage);
+end;
+
+
+procedure TBitsEhgkPageTestCase.TestLedOn;
+const
+  BitIndex: TEhgkPageIndex = 57;
+begin
+  AssertEquals('Initial value should be equals to 0', 0, EhgkPage.Value);
+
+  EhgkPage.Value.LedByIndexOn(1);
+  AssertEquals('Value for LED1 should be equals to 1', 1, EhgkPage.Value);
+
+  EhgkPage.Value.LedByIndexOn(2);
+  AssertEquals('Value for LED1 and LED2 should be equals to 3', 3, EhgkPage.Value);
+
+  EhgkPage.Value := 0;
+  EhgkPage.Value.LedByIndexOn(BitIndex);
+  AssertEquals(
+    'Value for LED57 should be equals to maximum',
+    $0100000000000000,
+    EhgkPage.Value
+  );
+end;
 
 
 initialization
 
   RegisterTest(TCreateEhgkPageTestCase);
+  RegisterTest(TBitsEhgkPageTestCase);
 end.
 
