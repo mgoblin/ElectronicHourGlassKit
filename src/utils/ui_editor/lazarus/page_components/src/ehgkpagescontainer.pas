525b2221c67a5a57af6fe511c0d31a638909f332
Michael Golovanov <mike.golovanov@gmail.com>
2026-06-16 07:33:25 +0300

    TEhgkPagesContainer constructor/desctructor implemented

--- src/utils/ui_editor/lazarus/page_components/src/ehgkpagescontainer.pas
+++ src/utils/ui_editor/lazarus/page_components/src/ehgkpagescontainer.pas
@@ -1,6 +1,7 @@
 unit EhgkPagesContainer;
 
 {$mode ObjFPC}{$H+}
+{$inline on}
 
 interface
 
@@ -15,13 +16,15 @@ type
   TEhgkPagesContainer = class(TComponent)
   private
     FPagesList: TEhgkPagesList;
-    function GetPageByIndex(Index: UInt8): TEhgkPage;
+    function GetPageByIndex(Index: UInt8): TEhgkPage; inline;
 
   protected
 
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
+
+    function Count: Integer;
 
     property Page[Index: UInt8]: TEhgkPage read GetPageByIndex;
   published
@@ -39,21 +42,33 @@ end;
 
 { TEhgkPagesContainer }
 
-function TEhgkPagesContainer.GetPageByIndex(Index: UInt8): TEhgkPage;
+function TEhgkPagesContainer.GetPageByIndex(Index: UInt8): TEhgkPage; inline;
 begin
   Result := FPagesList.Items[Index];
 end;
 
 constructor TEhgkPagesContainer.Create(AOwner: TComponent);
+var
+  EhgkPage: TEhgkPage;
 begin
   inherited Create(AOwner);
   FPagesList := TEhgkPagesList.Create();
+
+  // FPageList is responsible for destruction of its items.
+  // Container is not direct owner of pages
+  EhgkPage := TEhgkPage.Create(Nil);
+  FPagesList.Add(EhgkPage);
 end;
 
 destructor TEhgkPagesContainer.Destroy;
 begin
   FreeAndNil(FPagesList);
   inherited Destroy;
+end;
+
+function TEhgkPagesContainer.Count: Integer;
+begin
+  Result := FPagesList.Count;
 end;
 
 initialization
